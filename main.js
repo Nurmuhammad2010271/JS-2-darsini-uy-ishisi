940437354bed3de8c2e4dfd53403740b47986914
Nurmuhammad2010271 <938630762a@gmail.com>
2025-02-02 20:10:11 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let String1 = prompt("String typedagi malumot kriting");
+let Number1 = prompt("Number typedagi malumot kriting");
+let Boolean1 = prompt("Boolean typedagi malumot kriting");
+confirm("hamma malumotlarni to'g'ri kiritganizga ishonch komolmi?");
+alert(`Siz kiritgan ${String1} sozi string typega tegishli 
+Siz kiritgan ${Number1} sozi number typega tegishli
+Siz kiritgan ${Boolean1} sozi boolean typega tegishli`)
